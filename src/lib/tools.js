39c3500d75e16efc4406a94e30943c29ed1f1f59
Krzysztof Krztoń <k.krzton@cksource.com>
2021-01-14 15:20:57 +0100

    Say hello to 2021!

--- src/lib/tools.js
+++ src/lib/tools.js
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2012-2018, CKSource - Frederico Knabben. All rights reserved.
+ Copyright (c) 2012-2021, CKSource - Frederico Knabben. All rights reserved.
  For licensing, see LICENSE.md
  */
 
