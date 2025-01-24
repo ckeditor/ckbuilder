f79777e1ccdeb3b84fad8da5024313b4d3dfc4eb
KarolDawidziuk <karl1995000@wp.pl>
2025-01-24 11:52:59 +0100

    Update license headers to 2025

--- test/_assets/samples/ckeditor-dev/samples/old/sample_posteddata.php
+++ test/_assets/samples/ckeditor-dev/samples/old/sample_posteddata.php
@@ -9,7 +9,7 @@
   To save the content created with CKEditor you need to read the POST data on the server
   side and write it to a file or the database.
 
-  Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+  Copyright (c) 2003-2025, CKSource Holding sp. z o.o. All rights reserved.
   For licensing, see LICENSE.html or <a href="https://ckeditor.com/legal/ckeditor-oss-license/">https://ckeditor.com/legal/ckeditor-oss-license/</a>
 -------------------------------------------------------------------------------------------
 
